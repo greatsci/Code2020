32dbb566d09efb2168fbbc94d3052d2ca0151514
greatsci <ningz615@gmail.com>
2020-04-12 10:50:07 -0500

    Initial commit

---
+++ Valid para.py
@@ -0,0 +1,10 @@
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# The brackets must close in the correct order.
+
+
+def validP(num):
+    if num:
+        return 0
+
+
+
