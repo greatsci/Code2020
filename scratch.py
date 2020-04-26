e3ccfafac14ab089919370e227b47283597c348f
greatsci <ningz615@gmail.com>
2020-04-26 01:00:30 -0500

    test

--- scratch.py
+++ scratch.py
@@ -1,3 +1,19 @@
-a = 1
-if a != 0:
-    print('T')
+n = 13
+
+res = 0
+digit = n
+aug = 0
+# num = digit
+# while num // 10 != 0:
+#     aug += 1
+#     num = num // 10
+
+for i in range(n):
+    res += digit * 10**(i + aug)
+    # aug = 0
+    num = digit
+    while num // 10 != 0:
+        aug += 1
+        num = num // 10
+    digit -= 1
+print(res)
