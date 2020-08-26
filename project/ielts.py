549723cf3494bc018d042bb6c44cba138834dde5
greatsci <ningz615@gmail.com>
2020-08-26 14:48:48 -0500

    update

--- project/ielts.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import csv
-import requests
-import time
-
-url = 'https://www.ieltsusatest.org/IELTS?countryId=222&testCentreLocationId=1876'
-
-print(requests.get(url))
--- project/test.py
+++
@@ -1,9 +0,0 @@
-content = 'On May 15, 2020, we received your Form I-765, Application for Employment Authorization, Receipt Number YSC2090248025, and sent you the receipt notice that describes how we will process your case. Please follow the instructions in the notice. If you do not receive your receipt notice by June 14, 2020, contact the USCIS Contact Center at www.uscis.gov/contactcenter. If you move, go to www.uscis.gov/addresschange to give us your new mailing address.'
-# if content != 'sdf':
-#     print("sd\\ns")
-# else:
-#     print(0)
-
-s1 = content.split('On ')[1]
-s2 = s1.split(',')
-print(s2[0])
